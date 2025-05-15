f03568ba09cb9eeb3bfa6e647308e5bd79b861e4
Paulo R <pauloorbolato@cpejr.com.br>
2025-05-15 19:22:54 -0300

    fix: added models

---
+++ src/Models/UsuarioModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UsuarioSchema = new Schema({
+  email: String,
+  senha: String,
+  nome: String,
+  status: String,
+  cargo: String,
+});
+
+const UsuarioModel = new mongoose.model("usuarios", UsuarioScshema);
+
+module.exports = UsuarioModel;
