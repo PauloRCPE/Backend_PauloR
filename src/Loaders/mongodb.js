49c4a874b005d72d0053635000afb1ce1896ce64
Paulo R <pauloorbolato@cpejr.com.br>
2025-05-15 19:04:05 -0300

    fix: mongodb

--- src/Loaders/mongodb.js
+++ src/Loaders/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function startDB() {
   await mongoose.connect(
-    "mongodb+srv://pauloorbolato:0902Dragaoanciao~@nodebackend.b48rbqt.mongodb.net/?retryWrites=true&w=majority&appName=NodeBackend"
+    "mongodb+srv://pauloorbolato:0902dragao@nodebackend.b48rbqt.mongodb.net/?retryWrites=true&w=majority&appName=NodeBackend"
   );
 }
 module.exports = startDB;
